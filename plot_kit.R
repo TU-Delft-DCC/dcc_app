f27e9ee920e4787d9921057e088164166cdef8b6
alwil <alexandra.wilczynska@gmail.com>
2022-08-04 19:19:52 +0200

    added a few plots

---
+++ plot_kit.R
@@ -0,0 +1,14 @@
+# TU Delft Colors ---------------------------------------------------------
+
+tud_col_prim <- c('#00A6D6', '#FFFFFF' , '#000000')
+
+tud_blue <- c('#0C2340','#00B8C8','#0076C2')
+
+tud_pink <- c('#6F1D77','#EF60A3','#A50034')
+
+tud_orange <- c('#E03C31','#EC6842','#FFB81C')
+
+tud_green <- c('#6CC24A', '#009B77')
+
+tud_qual <- c('#00B8C8','#EC6842', '#FFB81C', '#6CC24A')
+
